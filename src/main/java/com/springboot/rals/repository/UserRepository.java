d600bcf319941da81abaaaef4365b1a1ea9a0c57
Nguyen Tuan Cuong (K17 HL) <cuongnthe171665@fpt.edu.vn>
2023-10-08 20:08:01 +0700

    Back-end implementation for Login Feature

--- src/main/java/com/springboot/rals/repository/UserRepository.java
+++ src/main/java/com/springboot/rals/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.springboot.rals.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    // tim user trong database bang email
+    User findByEmail(String email);
 }
--- src/main/java/com/springboot/rals/service/impl/UserServiceImpl.java
+++ src/main/java/com/springboot/rals/service/impl/UserServiceImpl.java
@@ -5,15 +5,24 @@ import com.springboot.rals.entity.User;
 import com.springboot.rals.repository.UserRepository;
 import com.springboot.rals.service.UserService;
 import com.springboot.rals.web.dto.UserRegistrationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
@@ -25,12 +34,20 @@ public class UserServiceImpl implements UserService {
         User user = new User(registrationDto.getFirstName(),
                 registrationDto.getLastName(),
                 registrationDto.getEmail(),
-                registrationDto.getPassword(), Arrays.asList(new Role("ROLES_USER")));
+                passwordEncoder.encode(registrationDto.getPassword()), Arrays.asList(new Role("ROLES_USER")));
         return userRepository.save(user);
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(username);
+        if (user == null)
+            throw new UsernameNotFoundException("Invalid email or password");
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    }
+
+    // map role to authorities
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 }
